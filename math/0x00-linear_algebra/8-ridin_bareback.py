0e4d7feea37f2fd06aff7c3501321e6ffbc0f9d8
FATIZ-CH <78651355+FATIZ-CH@users.noreply.github.com>
2021-03-07 21:53:32 +0100

    8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-import numpy
 def mat_mul(mat1, mat2):
-    return numpy.dot(mat1,mat2)
+    if len(mat1[0]) != len(mat2):
+        return None
+    else:
+        new_mat = []
+        for i in range(len(mat1)):
+            mat_i = []
+            for j in range(len(mat2[0])):
+                vec = 0
+                for k in range(len(mat2)):
+                    vec += mat1[i][k] * mat2[k][j]
+                mat_i.append(vec)
+            new_mat.append(mat_i)
+        for x in new_mat:
+            return new_mat
+
 mat_mul = __import__('8-ridin_bareback').mat_mul
 
 mat1 = [[1, 2],
