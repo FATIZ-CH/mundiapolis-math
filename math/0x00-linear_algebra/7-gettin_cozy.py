724b1f8a65f2da1a35172143e47ff2f1cec730b3
FATIZ-CH <78651355+FATIZ-CH@users.noreply.github.com>
2021-03-07 21:52:57 +0100

    7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
-import numpy
+
 def cat_matrices2D(mat1, mat2, axis=0):
-    return numpy.concatenate((mat1,mat2),axis)
+    if (len(mat1[0]) == len(mat2[0])) and axis == 0:
+        new_mat1 = [x[:] for x in mat1]
+        new_mat2 = [x[:] for x in mat2]
+        new_mat = new_mat1 + new_mat2
+        return new_mat
+    elif (len(mat1) == len(mat2)) and axis == 1:
+        new_mat = [mat1[x] + mat2[x] for x in range(len(mat1))]
+        return new_mat
+    else:
+        return None
 cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
 
 mat1 = [[1, 2], [3, 4]]
