d03980f7d5577cac47685847bc2ccc30c74bb3e4
FATIZ-CH <78651355+FATIZ-CH@users.noreply.github.com>
2021-03-12 14:33:33 +0100

    101-the_whole_barn.py

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 def add_matrices(mat1, mat2):
     if np.shape(mat1) != np.shape(mat1):
-        return None
-
+       return None
     result = np.add(mat1, mat2)
-    return result
-  
-  
+    return result  
 add_matrices = __import__('101-the_whole_barn').add_matrices
