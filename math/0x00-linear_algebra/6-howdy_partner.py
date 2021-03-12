d24a2a9e105042b0f8bba3a257be2eb397db9934
FATIZ-CH <78651355+FATIZ-CH@users.noreply.github.com>
2021-03-12 09:25:57 +0100

    6-howdy_partner.py

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
-
-
 def cat_arrays(arr1, arr2):
     arr3 = []
     arr3 = arr1 + arr2
     return arr3
-
-cat_arrays = __import__('6-howdy_partner').cat_arrays
-
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
