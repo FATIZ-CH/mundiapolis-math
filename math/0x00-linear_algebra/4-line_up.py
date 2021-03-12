3620c04c37ab5d136d33a3fcd4ca7c89871f9a76
FATIZ-CH <78651355+FATIZ-CH@users.noreply.github.com>
2021-03-12 09:17:19 +0100

    4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -6,11 +6,3 @@ def add_arrays(arr1, arr2):
             result.append(arr1[i] + arr2[i])
         return result
     return None
-add_arrays = __import__('4-line_up').add_arrays
-
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
